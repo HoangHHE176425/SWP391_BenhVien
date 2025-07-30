c683f7ea5f0570a46d0450d96b669ee5d2984dda
ohrax <iamyungrax@gmail.com>
2025-07-30 21:00:32 +0700

    sửa backend
    thêm model pharmacy transaction và prescription, sửa controller và route của medicine (thay delete thành disable)

---
+++ backend/models/PharmacyTransaction.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const pharmacyTransactionSchema = new mongoose.Schema({
+  prescription: { type: mongoose.Schema.Types.ObjectId, ref: 'Prescription' },
+  pharmacist: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Dược sĩ CÁI NÀY CỦA M, ADD NÓ R LẬP 1 CÁI CRD
+
+  patient: { type: mongoose.Schema.Types.ObjectId, ref: 'Patient' },
+
+  items: [
+    {
+      medicine: { type: mongoose.Schema.Types.ObjectId, ref: 'Medicine' },
+      quantity: Number,
+      price: Number
+    }
+  ],
+
+  totalAmount: Number,
+  paid: { type: Boolean, default: false },
+  paymentMethod: { type: String, enum: ['cash', 'insurance', 'online'] },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('PharmacyTransaction', pharmacyTransactionSchema);
