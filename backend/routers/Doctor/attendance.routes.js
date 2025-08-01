62ffc5d917c56fb426b13de01a3d1d19e44dc52c
hoangHHE176425 <hoangxmhg2003@gmail.com>
2025-08-01 07:26:58 +0700

    Revise attendance routes to support filtering by date and department

--- backend/routers/Doctor/attendance.routes.js
+++ backend/routers/Doctor/attendance.routes.js
@@ -5,10 +5,8 @@ const attendanceController = require("../../controller/doctor/attendanceDoctor")
 router.post('/check-in', attendanceController.checkIn);
 router.put('/check-out', attendanceController.checkOut);
 router.get('/employee/:employeeId', attendanceController.getAttendanceByEmployee);
-router.get('/', attendanceController.getAllAttendance);
-router.get("/history/:employeeId", attendanceController.getAttendanceHistory);
+router.get('/history/:employeeId', attendanceController.getAttendanceHistory);
 router.get('/today-schedule/:employeeId', attendanceController.getTodaySchedule);
-router.get("/all-schedules/:employeeId", attendanceController.getDoctorSchedules);
-router.get('/all-schedules/:employeeId', attendanceController.getAllSchedules);
+router.get('/schedules/all/:employeeId', attendanceController.getAllSchedules);
 
 module.exports = router;
