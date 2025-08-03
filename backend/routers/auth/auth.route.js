be2e757edb3e31cd2c40ddc634fd17972de21aa9
hoangHHE176425 <hoangxmhg2003@gmail.com>
2025-08-03 15:44:50 +0700

    Adjust routing in auth.route.js

--- backend/routers/auth/auth.route.js
+++ backend/routers/auth/auth.route.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const authRouter = express.Router();
-const { Login, Signup, check, changePassword, forgotPassword, resetPassword } = require('../../controller/auth/authServices');
+const { Login, Signup, check, changePassword, forgotPassword, resetPassword, googleLogin, googleCompleteRegister, checkPhone,} = require('../../controller/auth/authServices');
 const { authMiddleware, ismeomeo } = require('../../middleware/auth.middleware');
+
 authRouter.post('/login', Login);
 authRouter.post('/signup', Signup);
 authRouter.post('/updatePassword', changePassword);
+
 // authRouter.post('/authMiddleware', authMiddleware, ismeomeo, check);
 authRouter.post('/forgotPassword', forgotPassword);
 authRouter.post('/resetPassword', resetPassword);
+authRouter.post('/google-login', googleLogin);
+authRouter.post('/google-complete-register', googleCompleteRegister);
+authRouter.post("/check-phone", checkPhone);
+
 module.exports = authRouter;
