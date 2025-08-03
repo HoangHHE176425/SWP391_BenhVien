cfd772be76cebbf1fdf630e2562207cb988ccfaa
hoangHHE176425 <hoangxmhg2003@gmail.com>
2025-08-04 04:07:12 +0700

    Revise invoice model structure for clarity

--- backend/models/Invoice.js
+++ backend/models/Invoice.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
 
 const invoiceSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false, index: true },
-  profileId: { type: mongoose.Schema.Types.ObjectId, ref: 'Profile', required: false },
+  profileId: { type: mongoose.Schema.Types.ObjectId, ref: 'Profile', required: true },
+  recordIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Records' }],
   paymentId: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Payment' }],
-  invoiceNumber: { type: String, required: true, unique: true }, // đủ để tạo index
+  invoiceNumber: { type: String, required: true, unique: true },
   services: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Services' }],
+  medicines: [{
+    medicine: { type: mongoose.Schema.Types.ObjectId, ref: 'Medicine' },
+    quantity: { type: Number, required: true }
+  }],
   totalAmount: { type: Number, required: true, min: 0 },
   status: { type: String, enum: ['Pending', 'Paid', 'Canceled'], default: 'Pending' }
 }, { timestamps: true });
-
-// invoiceSchema.index({ invoiceNumber: 1 });
 
 module.exports = mongoose.model('Invoice', invoiceSchema);
